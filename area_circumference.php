dcd644662b7d60da25e9717f368b5762117c3518
Joserafaelnb <joserafaelnbezerra@gmail.com>
2022-05-03 20:08:33 -0300

    working

---
+++ area_circumference.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>Document</title>
+</head>
+<body>
+
+<form action="area_circumference.php" method="get">
+
+<p><input type="text" name="R" id=""></p>
+<p><input type="submit" value="submit"></p>
+
+
+</form>
+
+    
+<?php
+
+
+?>
+
+
+</body>
+</html>
