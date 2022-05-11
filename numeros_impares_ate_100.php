5fad30f5e65fa90c748b6ea25d508ecdf81295a6
Joserafaelnb <joserafaelnbezerra@gmail.com>
2022-05-11 12:01:26 -0300

    refatorando codigo php

--- numeros_impares_ate_100.php
+++ numeros_impares_ate_100.php
@@ -1,24 +1,37 @@
 
-
-
-
 <?php
- 
- const parada =  100;
+
+const parada =  100;
 
 $numero_impar = 0;
- 
- for( $cont =0 ; $cont <= parada; $cont++){
 
-    NumeroPar($numero_impar++);
+$listaNumeros = array();
 
- }
 
-function NumeroPar($numero_impar){
-    if($numero_impar%2 == 1 ){
-        echo("$numero_impar \n");
-     
-         }
+for( $cont =0 ; $cont <= parada; $cont++){
+
+  if(NumeroImpar($numero_impar)){
+      $listaNumeros[] = $numero_impar;
+
+  }
+        $numero_impar++;
 }
+
+
+
+foreach ($listaNumeros as $list){
+    echo "$list \n";
+}
+
+
+function NumeroImpar($numero_impar){
+
+
+    return ($numero_impar%2 == 1) ? true : false;
+
+
+}
+
+
 
 
